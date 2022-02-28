8fa67465ff27d2ed39d1898d248bfdd5277c323b
Franz Cao <franzdcao@gmail.com>
2022-02-27 23:47:10 -0600

    Invalid Text Fix

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const startGameBtn = document.getElementById("start-game-btn");
+const startGameBtn = document.getElementById('start-game-btn');
 
-const ROCK = "ROCK";
-const PAPER = "PAPER";
-const SCISSORS = "SCISSORS";
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = ROCK;
-const RESULT_DRAW = "DRAW";
-const RESULT_PLAYER_WINS = "PLAYER_WINS";
-const RESULT_COMPUTER_WINS = "COMPUTER_WINS";
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
 
 let gameIsRunning = false;
 
 const getPlayerChoice = () => {
   const selection = prompt(
     `${ROCK}, ${PAPER}, or ${SCISSORS}?`,
-    ""
+    ''
   ).toUpperCase();
   if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
     alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
-    return DEFAULT_USER_CHOICE;
+    return;
   }
   return selection;
 };
@@ -33,7 +33,7 @@ const getComputerChoice = () => {
   }
 };
 
-const getWinner = (cChoice, pChoice) =>
+const getWinner = (cChoice, pChoice = DEFAULT_USER_CHOICE) =>
   cChoice === pChoice
     ? RESULT_DRAW
     : (cChoice === ROCK && pChoice === PAPER) ||
@@ -54,23 +54,60 @@ const getWinner = (cChoice, pChoice) =>
 //   return RESULT_COMPUTER_WINS;
 // }
 
-startGameBtn.addEventListener("click", () => {
+startGameBtn.addEventListener('click', () => {
   if (gameIsRunning) {
     return;
   }
   gameIsRunning = true;
-  console.log("Game is starting...");
-  const playerChoice = getPlayerChoice();
+  console.log('Game is starting...');
+  const playerChoice = getPlayerChoice(); //might be undefined
   const computerChoice = getComputerChoice();
-  const winner = getWinner(computerChoice, playerChoice);
-  let message = `You picked ${playerChoice}, computer picked ${computerChoice}, therefore you `;
-  if (winner === RESULT_DRAW) {
-    message = message + "had a draw.";
-  } else if (winner === RESULT_PLAYER_WINS) {
-    message = message + "won.";
+  let winner;
+  if (playerChoice) {
+    winner = getWinner(computerChoice, playerChoice);
   } else {
-    message = message + "lost.";
+    winner = getWinner(computerChoice);
+  }
+  let message = `You picked ${
+    playerChoice || DEFAULT_USER_CHOICE
+  }, computer picked ${computerChoice}, therefore you `;
+  if (winner === RESULT_DRAW) {
+    message = message + 'had a draw.';
+  } else if (winner === RESULT_PLAYER_WINS) {
+    message = message + 'won.';
+  } else {
+    message = message + 'lost.';
   }
   alert(message);
   gameIsRunning = false;
 });
+
+// not related to game
+
+const sumUp = (resultHandler, ...numbers) => {
+  const validateNumber = (number) => {
+    return isNaN(number) ? 0 : number;
+  };
+
+  let sum = 0;
+  for (const num of numbers) {
+    sum += validateNumber(num);
+  }
+  resultHandler(sum);
+};
+
+const subtractUp = function () {
+  let sum = 0;
+  for (const num of arguments) {
+    //don't use that
+    sum -= num;
+  }
+  return sum;
+};
+
+const showResult = (result) => {
+  alert('The result after adding all numbers is: ' + result);
+};
+
+sumUp(showResult, 1, 5, 10, 'sdfsd', -3, 6, 10);
+console.log(subtractUp(1, 5, 10, -3, 6, 10));
